69779b296073c8547244d3368570416654086149
beketkus <117732239+beketkus@users.noreply.github.com>
2022-12-14 02:44:57 +0600

    Create WordDivider.cpp

---
+++ WordDivider.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void WordDivider(const &string str){
+	string n,k;
+	n = str;
+	int i = 0, cnt = 1;
+	
+	for(int j = 0; j < n.size(); j++) if(n[j] == ' ') cnt++;
+	for(int j = 0; j < cnt; j++){
+	string k = "";
+	while(n[i] != ' ' and i < n.size()){
+		k += n[i];
+		i++;
+		}
+	i++;
+	}
+}
+	
